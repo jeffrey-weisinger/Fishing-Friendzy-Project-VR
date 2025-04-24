d4ba65d8020f65ec9f6e636057fec504afc0a062
Jeff <jeffreyweisinger@gmail.com>
2025-04-24 17:12:24 -0500

    Can now move rod

--- Assets/RightHandInputRouter.cs
+++ Assets/RightHandInputRouter.cs
@@ -1,3 +1,29 @@
+// using UnityEngine;
+
+
+// public class RightHandInputRouter : MonoBehaviour
+// {
+//     public UnityEngine.XR.Interaction.Toolkit.Interactors.XRRayInteractor interactor;
+
+//     public void OnGrab()
+//     {
+//         Debug.Log("A button pressed!");
+
+//         // Use the first selected interactable, if any
+//         var interactable = interactor.interactablesSelected.Count > 0
+//             ? interactor.interactablesSelected[0]
+//             : null;
+
+//         if (interactable != null)
+//         {
+//             interactor.interactionManager.SelectEnter(
+//                 (UnityEngine.XR.Interaction.Toolkit.Interactors.IXRSelectInteractor)interactor,
+//                 (UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable)interactable
+//             );
+//         }
+//     }
+// }
+
 using UnityEngine;
 
 
@@ -9,17 +35,15 @@ public class RightHandInputRouter : MonoBehaviour
     {
         Debug.Log("A button pressed!");
 
-        // Use the first selected interactable, if any
-        var interactable = interactor.interactablesSelected.Count > 0
-            ? interactor.interactablesSelected[0]
+        // Check what the ray is HOVERING OVER
+        var hoveredInteractable = interactor.interactablesHovered.Count > 0
+            ? interactor.interactablesHovered[0]
             : null;
 
-        if (interactable != null)
-        {
-            interactor.interactionManager.SelectEnter(
-                (UnityEngine.XR.Interaction.Toolkit.Interactors.IXRSelectInteractor)interactor,
-                (UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable)interactable
-            );
+        if (hoveredInteractable != null && hoveredInteractable is UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable selectableInteractable)
+        { 
+            // Cast to the correct interface type
+            interactor.interactionManager.SelectEnter(interactor, selectableInteractable);
         }
     }
 }
